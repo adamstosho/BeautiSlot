6d9054c4b907710d9102f63b338fe0234dcd7ba4
adamstosho <omoridoh111@gmail.com>
2025-07-22 17:16:34 +0100

    cors operation handling and vercel  routes configuration in the frontend

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,10 @@ const swaggerDocument = require('./config/swagger.json');
 
 const app = express();
 
-// Security Middlewares
 app.use(helmet());
 const allowedOrigins = process.env.CORSWHITELIST ? process.env.CORSWHITELIST.split(',') : [];
 app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
@@ -30,12 +28,23 @@ app.use(morgan('dev'));
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 500, 
 });
 app.use(limiter);
 
-// Placeholder for API routes
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
 const authRoutes = require('./routes/auth.routes');
 app.use('/api/auth', authRoutes);
 const providerRoutes = require('./routes/provider.routes');
@@ -52,12 +61,10 @@ const adminRoutes = require('./routes/admin.routes');
 app.use('/api/admin', adminRoutes);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// 404 Handler
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Global Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
